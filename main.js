7e579aa0138fa210de717d64ce2718ec8f211c7a
ItsTobez <mrtobez08@gmail.com>
2021-07-07 15:37:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.scene').forEach((elem) => {
+
+  const modifier = elem.getAttribute('data-modifier')
+
+  basicScroll.create({
+    elem: elem,
+    from: 0,
+    to: 519,
+    props: {
+      '--translateY': {
+        from: '0',
+        to: `${ 10 * modifier }px`,
+        direct: true
+      }
+    }
+  }).start()
+
+})
+
+document.documentElement.style.setProperty('--color', randomColor())
